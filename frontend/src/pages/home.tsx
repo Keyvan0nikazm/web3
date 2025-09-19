a16caf25dee27433dd4f0b61b209cf16e854d970
Keyvan <keyvan.nikazm@gmail.com>
2025-09-19 10:14:45 +0200

    add changes

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,37 +1,40 @@
+import { useState } from 'react';
 import ExpenseItem from '../components/ExpensiveItem';
 import type { Expense } from '../type'
 
 const Home = () => {
-  const expenses: Expense[] = [
+  const initialExpense: Expense[] = [
     {
       id: '1',
       description: 'Courses alimentaires',
       amount: 85.50,
       date: '2025-09-18',
-      pay: 'Carte bancaire'
+      pay: 'Alice'
     },
     {
       id: '2',
       description: 'Essence voiture',
       amount: 65.00,
       date: '2025-09-17',
-      pay: 'Espèces'
+      pay: 'Bob'
     },
     {
       id: '3',
       description: 'Restaurant',
       amount: 42.75,
       date: '2025-09-16',
-      pay: 'Carte bancaire'
+      pay: 'Dani'
     },
     {
       id: '4',
       description: 'Abonnement Netflix',
       amount: 15.99,
       date: '2025-09-15',
-      pay: 'Prélèvement automatique'
+      pay: 'Ethan'
     }
   ];
+
+  const [expenses] = useState<Expense[]>(initialExpense);
 
   return (
     <div className="home-page">
